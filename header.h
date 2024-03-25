70911ed19de42aa20d40cfd7fc8528bdf35a20b6
Valentin Maupas <val.maupas@gmail.com>
2024-03-25 12:55:35 +0100

    Deuxieme commit

---
+++ header.h
@@ -0,0 +1,18 @@
+//
+// Created by valma on 25/03/2024.
+//
+
+
+#ifndef ECECOOKED_HEADER_H
+#define ECECOOKED_HEADER_H
+
+#include <allegro.h>
+#include <stdio.h>
+
+#define SCREEN_W 800
+#define SCREEN_H 600
+
+
+
+
+#endif //ECECOOKED_HEADER_H
--- main.c
+++ main.c
@@ -1,8 +1,15 @@
-//
-// Created by valma on 15/03/2024.
-//
-#include <stdio.h>
+#include "header.h"
 
 void main() {
-    printf("Hello world");
-}
+    allegro_init();
+    install_keyboard();
+    set_color_depth(desktop_color_depth());
+    if((set_gfx_mode(GFX_AUTODETECT_WINDOWED,800,800,0,0))!=0)
+    { 	allegro_message("Pb de mode graphique") ;
+        allegro_exit();
+        exit(EXIT_FAILURE);
+    }
+    int c =  makecol(0, 255, 0);
+    rect(screen, 80 , 260, 160, 300, c);
+
+}END_OF_MAIN()
